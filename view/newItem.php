27ce60809351a78ff107e4f70272ad3c6b664fb8
Jacklee <jack19820814@gmail.com>
2017-11-12 23:39:37 +0800

    add a form for new item

--- view/newItem.php
+++ view/newItem.php
@@ -7,6 +7,29 @@
   </head>
   <body>
 
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <form class="" action="newItem.html" method="post">
+            <div class="form-group">
+              <label for="Class">Class</label>
+              <select class="form-control" id="selectClass">
+                <option disabled selected>Choose the class</option>
+                <option>First of class</option>
+                <option>Second of class</option>
+                <option>Third of class</option>
+              </select>
+            </div>
+            <div class="form-group">
+              <label for="description">Description</label>
+              <textarea class="form-control" id="ItemDescription" rows="3"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </form>
+        </div>
+      </div>
+    </div>
+
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
